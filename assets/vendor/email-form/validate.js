cb0cba9329eb3362dba0466f39d4e8367c9fff59
Maryll Castelino <maryllcastelino@gmail.com>
2022-07-04 21:44:00 +0530

    added google form backend to contact me form

--- assets/vendor/email-form/validate.js
+++ assets/vendor/email-form/validate.js
@@ -1,12 +1,12 @@
 /**
 * PHP Email Form Validation - v3.2
-* URL: https://bootstrapmade.com/php-email-form/
+* URL: https://bootstrapmade.com/email-form/
 * Author: BootstrapMade.com
 */
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('.email-form');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
@@ -52,24 +52,25 @@
   function php_email_form_submit(thisForm, action, formData) {
     fetch(action, {
       method: 'POST',
+      mode: 'no-cors',
       body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+      headers: {}
     })
-    .then(response => {
-      if( response.ok ) {
-        return response.text()
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
+    // .then(response => {
+    //   if( response.ok ) {
+    //     return response.text()
+    //   } else {
+    //     throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+    //   }
+    // })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      // if (data.trim() == 'OK') {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
+      // } else {
+      //   throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+      // }
     })
     .catch((error) => {
       displayError(thisForm, error);
--- forms/contact.php
+++ forms/contact.php
@@ -2,14 +2,14 @@
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
+  * The library should be uploaded to: vendor/email-form/email-form.php
+  * For more info and help: https://bootstrapmade.com/email-form/
   */
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/email-form/email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
